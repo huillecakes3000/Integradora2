90ced1bb89ad8337d3f022f41d3a3694f4c52275
Guillermo Vergara <323guillermo.vergara.aguirre@gmail.com>
2024-07-03 17:23:36 -0600

    MENU TAB AGREGADO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,55 +3,46 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'landing',
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'landing',
-    loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingPageModule)
-  },
-  {
-    path: 'reset-pasword',
-    loadChildren: () => import('./pages/reset-pasword/reset-pasword.module').then( m => m.ResetPaswordPageModule)
-  },
-  {
-    path: 'email-send',
-    loadChildren: () => import('./pages/email-send/email-send.module').then( m => m.EmailSendPageModule)
+    loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingPageModule)
   },
   {
     path: 'correoalready',
-    loadChildren: () => import('./pages/correoalready/correoalready.module').then( m => m.CorreoalreadyPageModule)
+    loadChildren: () => import('./pages/correoalready/correoalready.module').then(m => m.CorreoalreadyPageModule)
+  },
+  {
+    path: 'email-send',
+    loadChildren: () => import('./pages/email-send/email-send.module').then(m => m.EmailSendPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'notfound',
-    loadChildren: () => import('./pages/notfound/notfound.module').then( m => m.NotfoundPageModule)
+    loadChildren: () => import('./pages/notfound/notfound.module').then(m => m.NotfoundPageModule)
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'reset-pasword',
+    loadChildren: () => import('./pages/reset-pasword/reset-pasword.module').then(m => m.ResetPaswordPageModule)
   },
   {
-    path: 'status-ground',
-    loadChildren: () => import('./status-ground/status-ground.module').then( m => m.StatusGroundPageModule)
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupPageModule)
   },
   {
-    path: 'porfile',
-    loadChildren: () => import('./porfile/porfile.module').then( m => m.PorfilePageModule)
-  },
- 
+    path: '**',
+    redirectTo: 'notfound'
+  }
 ];
 
 @NgModule({
@@ -60,4 +51,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
