45a71fabb4c49ea0bc6f77ec08168c802c0c6231
Guillermo Vergara <323guillermo.vergara.aguirre@gmail.com>
2024-07-03 19:04:03 -0600

    changes in porfile section - create section

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { AlertButton } from '@ionic/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  alertButtons: AlertButton[] = [
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      cssClass: 'secondary',
+    },
+    {
+      text: 'Aceptar',
+      handler: () => {
+        console.log('Botón Aceptar presionado');
+        // Aquí puedes agregar la lógica que deseas ejecutar cuando se presiona el botón Aceptar
+      },
+    },
+  ];
 
   constructor() {}
+  
 
+  // Método que maneja el evento didDismiss del alert
+  setResult(event: any) {
+    console.log('Evento didDismiss:', event);
+    // Aquí puedes manejar el resultado del alert
+  }
 }
